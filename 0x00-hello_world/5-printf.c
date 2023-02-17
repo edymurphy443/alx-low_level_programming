704269e21088e4a25a69e3ad80ec547d79ed5389
edymurphy443 <edymurphy443@gmail.com>
2023-02-17 01:32:08 -0800

    1-compiler

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h
-/** 
+#include <stdio.h>
+/**
  * main - print the code using printf
  * Rturn: 0 if (success)
  */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
